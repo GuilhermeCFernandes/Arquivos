163fbc7e0ddb05767d6815c12480b7382aba12f1
GuilhermeCFernandes <guilherme.correa.fernandes255@gmail.com>
2016-04-25 09:49:46 -0300

    added super-widget 2.0.

--- iscripeti.py
+++ iscripeti.py
@@ -3,10 +3,7 @@ import re
 expr = re.compile(r"[^\>]+\>([^\<]+)\<");
 f = open("output.txt", "r")
 
-while True:
-    l = f.read()
-    if not l:
-        break
+for l in f:
     m = re.match(expr, l)
     if m:
         print(m.group(1))
